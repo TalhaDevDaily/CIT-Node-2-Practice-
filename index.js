cbf4e9f118081d14edd51d44d3a84f007634bcf4
TalhaDevDaily <talha@boishakhikunja.onmicrosoft.com>
2025-11-22 22:19:09 +0600

    Hey

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hey there!");
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send(
+    "Oh, what a beautiful morning, Oh, what a beautiful day, I've got a beautiful feeling, everything's going my way! 🌄"
+  );
+});
+
+app.post("/login", function (req, res) {
+  console.log(req);
+  res.send("Login Route 😎");
+});
+
+app.listen(8000, function () {
+  console.log("Server is running 🏃‍♂️");
+});
+
+// Request => getting data from the client side
