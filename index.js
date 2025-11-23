649fd24f17e48c76bad2c5c9f2340ffa96e34174
TalhaDevDaily <talha@boishakhikunja.onmicrosoft.com>
2025-11-23 10:43:05 +0600

    Done

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", function (req, res) {
   res.send(
     "Oh, what a beautiful morning, Oh, what a beautiful day, I've got a beautiful feeling, everything's going my way! 🌄"
@@ -9,8 +11,16 @@ app.get("/", function (req, res) {
 });
 
 app.post("/login", function (req, res) {
-  console.log(req);
-  res.send("Login Route 😎");
+  const { email, password } = req.body;
+
+  if (!email)
+    return res.status(401).send({ error: "Please fill in the email 📧" });
+
+  if (!password)
+    return res.status(401).send({ error: "Password is required 🔑" });
+  console.log(email);
+  console.log(password);
+  res.status(202).send({ success: `Successfully logged in to ${email} 😎` });
 });
 
 app.listen(8000, function () {
@@ -18,3 +28,5 @@ app.listen(8000, function () {
 });
 
 // Request => getting data from the client side
+
+// Respond => send data to the client side
